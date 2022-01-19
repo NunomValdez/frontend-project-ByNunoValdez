57e8352662d8c6a061218e334bafdab8bcae0b3a
Nuno Valdez <nunomvmf@gmail.com>
2022-01-19 18:20:33 +0000

    some changes - createContext

--- src/App.js
+++ src/App.js
@@ -2,30 +2,41 @@ import './App.css';
 import "@material-tailwind/react/tailwind.css";
 import HomePage from './pages/homePage/HomePage';
 import Checkout from './pages/checkout/Checkout';
-// import { useReducer } from 'react';
+import { createContext, useState } from 'react';
 import {
   BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom"
 
+
 //o context tem de ser definido aqui, para q os vários states sejam globais...! (?)
 //fazendo createContext, podemos usar a tag ...Provider para providenciar states/props
 // globais aos componentes filhos, e assim passar info de um lado para o outro
 
-// const [state, dispatch] = useReducer(reducer, initialState, init)
+//agora q fiz o context para todos os filhos de App.js, posso passar os states dos componentes aqui 
+//e os devidos componentes vão usar os states definidos aqui neste App, embora os usem lá dentro de si mesmos 
+//através do useContext --> usar contexto dentro do elemento, contexto esse q é criado no pai de todos!
+
+
+export const AppContext = createContext();
 
 
 export default function App() {
+
+  // const [changeCheckbox, setChangeCheckbox] = useState(false);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <>
-          <Route path="/" element={<HomePage />} /> 
-          <Route path="/checkout" element={<Checkout />} />
-        </>
-      </Routes>
-    </BrowserRouter>
+  <AppContext.Provider value={'x'}>
+      <BrowserRouter>
+        <Routes>
+          <>
+            <Route path="/" element={<HomePage />} /> 
+            <Route path="/checkout" element={<Checkout />} />
+          </>
+        </Routes>
+      </BrowserRouter>
+  </AppContext.Provider>
 
   );
 }
--- src/components/compo.tailwind/Checkbox.jsx
+++ src/components/compo.tailwind/Checkbox.jsx
@@ -1,35 +1,39 @@
 import React, { useState } from "react";
 import Checkbox from "@material-tailwind/react/Checkbox"
+// import { AppContext } from "../../App";
+// import { useContext } from "react";
+
 
 export default function CheckboxExtra(props) {
     let {extra, id} = props; //extraindo a prop extra e id, das props q vêm do pai modalDetails
 
-    const [change, setChange] = useState(
-        false
-        // new Array(extra.length).fill(false)
-    );
-    //o estado inicial será false, para q ao clicar fique a true, para ficar checked
+    const [changeCheckbox, setChangeCheckbox] = useState(false);
+    // o estado inicial será false, para q ao clicar fique a true, para ficar checked
 
 //    console.log(extra);
 
-    const handleChange= (event)=>{
-        console.log(`${event.target.checked} do ${extra}`)
-        setChange(!change)
+    // const { changeCheckbox, setChangeCheckbox } = useContext(AppContext);
 
+
+    const handleChangeCheckbox= (event)=>{
+        console.log(`${event.target.checked} do ${extra}`)
+        setChangeCheckbox(!changeCheckbox)
+        // console.log(changeCheckbox)
     }
 
     return (
         <Checkbox
+            // className="p-6"
             color="lightGreen"
             text={extra}
             value={extra}
             id={`${id}-${extra}`}  //o id é do cartao, e o extra do extra relativo ao id 
-            checked={change}
-            onChange={ handleChange }
+            checked={changeCheckbox}
+            onChange={ handleChangeCheckbox }
         />
     )
 }
 
 // com o value igual ao extra e value a true, mando para o carrinho c um filter:
-// se change=true e value=extra, envia po carrinho OU:
+// se changeCheckbox=true e value=extra, envia po carrinho OU:
 // se event.target.checked == true, manda po carrinho
